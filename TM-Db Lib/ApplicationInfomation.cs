6cf52f6244222a1e95ae557224def07f728ba47a
Tommo J. Phillips <tommojphillips@gmail.com>
2019-11-24 17:00:57 +1000

    Add project files.

---
+++ TM-Db Lib/ApplicationInfomation.cs
@@ -0,0 +1,39 @@
+﻿namespace TM_Db_Lib
+{
+    /// <summary>
+    /// Represents data used by all parts of the application.
+    /// </summary>
+    public static class ApplicationInfomation
+    {
+        // Written, 20.04.2018
+
+        /// <summary>
+        /// Represents my (Tommo J. Armytage) api key for connecting to The TMDb API.
+        /// </summary>
+        public const string API_KEY = "70b2d4ba8f50d9f2c4bd12c87cae3b68";
+        /// <summary>
+        /// Represents the search base address.
+        /// </summary>
+        public const string SEARCH_BASE_ADDRESS = "https://api.themoviedb.org/3/search";
+        /// <summary>
+        /// Represents the base url for certifications.
+        /// </summary>
+        public const string CERTIFICATIONS_BASE_ADDRESS = "https://api.themoviedb.org/3/certification";
+        /// <summary>
+        /// Represents the base url address for movies.
+        /// </summary>
+        public const string MOVIE_BASE_ADDRESS = "https://api.themoviedb.org/3/movie";
+        /// <summary>
+        /// Represents the base url address for tv series.
+        /// </summary>
+        public const string TV_BASE_ADDRESS = "https://api.themoviedb.org/3/tv";
+        /// <summary>
+        /// Represents the base url address for images.
+        /// </summary>
+        public const string IMAGE_BASE_ADDRESS = "https://image.tmdb.org/t/p/original";
+        /// <summary>
+        /// Represents the base url address for networks.
+        /// </summary>
+        public const string NETWORK_BASE_ADDRESS = "https://api.themoviedb.org/3/network";
+    }
+}
---
+++ TM-Db Lib/FileReference.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+
+namespace TM_Db_Lib
+{
+    /// <summary>
+    /// Represents an abstract class that holds infomation about a reference to a file.
+    /// </summary>
+    internal abstract class FileReference
+    {
+        // Written, 24.11.2019
+
+        #region Properties
+
+        /// <summary>
+        /// Represents the file location.
+        /// </summary>
+        internal virtual string path
+        {
+            get;
+            private set;
+        }
+        /// <summary>
+        /// Represents the file name. Gets the filename from the file location (<see cref="this.path"/>) provided.
+        /// </summary>
+        internal string fileName
+        {
+            get
+            {
+                return Path.GetFileNameWithoutExtension(this.path);
+            }
+        }
+
+        internal string fileExtention
+        {
+            get
+            {
+                return Path.GetExtension(this.path);
+                    }
+        }
+
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="FileReference"/> and sets <see cref="this.path"/> to the parameter, <paramref name="path"/>.
+        /// </summary>
+        /// <param name="path">the path to set.</param>
+        internal FileReference(string inPath)
+        {
+            // Written, 13.04.2018
+
+            this.path = inPath;
+        }
+
+        #endregion
+
+        #region Methods 
+
+        /// <summary>
+        /// Sets the path for the instance.
+        /// </summary>
+        /// <param name="inPath">The file location to set the path to.</param>
+        internal void setPath(string inPath)
+        {
+            // Written, 20.04.2018
+
+            this.path = inPath;
+        }
+
+        #endregion
+    }
+}
